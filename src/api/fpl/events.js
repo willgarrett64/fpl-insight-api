c436aa9351dd01698e2e88860ff56ddf68ce975e
Will <willgarrett64@yahoo.com>
2022-08-13 03:30:31 +0100

    feat: add events to fpl api

---
+++ src/api/fpl/events.js
@@ -0,0 +1,19 @@
+import fplCache from '../../cache.js'
+import { getPageResults } from '../../utils/pagination.js'
+
+// get event by id
+export const getEvent = () => async (req, res) => {
+  const events = await fplCache.get('events')
+  const id = Number(req.params.id)
+  const event = events.find(event => event.id === id)
+  res.send(event)
+}
+
+// get events
+export const getEvents = () => async (req, res) => {
+  let events = await fplCache.get('events')
+  // options
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 20
+  res.send(getPageResults(events, page, limit))
+}
--- src/api/fpl/index.js
+++ src/api/fpl/index.js
@@ -1,21 +1,30 @@
 import express from 'express'
+import { getEvent, getEvents } from './events.js'
+import { getFixture, getFixtures, getFixturesByEvent, getFixturesByTeam } from './fixtures.js'
 import { getPlayer, getPlayers } from './players.js'
+import { getTeam, getTeams } from './teams.js'
 
 const fplRouter = express.Router()
 
+// events
+fplRouter.get('/event/:id', getEvent())
+fplRouter.get('/events', getEvents())
+
 // fixtures
+fplRouter.get('/fixture/:id', getFixture())
 fplRouter.get('/fixtures', getFixtures())
-fplRouter.get('/fixtures/:id', getFixture())
+fplRouter.get('/fixtures/event/:event', getFixturesByEvent())
+fplRouter.get('/fixtures/team/:team', getFixturesByTeam())
 
 // players (premier league players)
-fplRouter.get('/players', getPlayers())
 fplRouter.get('/player/:id', getPlayer())
+fplRouter.get('/players', getPlayers())
 
 // squads (user created fpl squads)
 // fplRouter.get('/squad/:id', getSquad())
 
 // teams (premier league teams)
-fplRouter.get('/teams', getTeams())
 fplRouter.get('/team/:id', getTeam())
+fplRouter.get('/teams', getTeams())
 
 export default fplRouter
