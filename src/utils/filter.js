3e25d7dcbb86109a554f2086777381abdd0d2829
Will <willgarrett64@yahoo.com>
2022-08-31 19:16:39 +0100

    feat: update filters

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -6,15 +6,15 @@ export const filterPlayers = (req, allPlayers) => {
     let players = [...allPlayers]
 
     // filters
-    const positions = req.query.pos
-    const teams = req.query.team
+    const positions = req.query.pos && req.query.pos.split(',')
+    const teams = req.query.teams && req.query.teams.split(',')
     const minPrice = req.query.minPrice || 0
     const maxPrice = req.query.maxPrice || 20
     const minTsb = req.query.minTsb || 0
     const maxTsb = req.query.maxTsb || 100
 
-    if (positions) players = players.filter(player => positions.includes(player.position))
-    if (teams) players = players.filter(player => teams.includes(player.team))
+    if (positions) players = players.filter(player => positions.includes(player.element_type.toString()))
+    if (teams) players = players.filter(player => teams.includes(player.team.toString()))
     players = players.filter(player => {
       const price = Number(player.now_cost)
       const tsb = Number(player.selected_by_percent)
